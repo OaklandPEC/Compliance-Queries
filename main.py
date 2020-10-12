c6959e710196e6c2c0d7f2db897b09cc8a715305
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2020-10-12 13:57:55 -0700

    figured out how to convert API data string to list

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import requests
+import json
 
 scheduleA_api = 'https://data.oaklandca.gov/resource/3xq4-ermg.json'
 
 response = requests.get(scheduleA_api)
 
-print(response.text)
+scheduleA_dict = response.json()
+
+for item in scheduleA_dict:
+    print(item['filer_id'])
+
+
