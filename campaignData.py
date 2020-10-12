992149f1db693d59ece227f6d3f0e59ddc40f448
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2020-10-12 14:53:29 -0700

    Added CSV functionality

---
+++ campaignData.py
@@ -0,0 +1,11 @@
+#itemized cash contributions
+monetary = 'https://data.oaklandca.gov/dataset/Campaign-Finance-FPPC-Form-460-Schedule-A-Monetary/3xq4-ermg'
+
+#in-kind contributions
+non_monetary = 'https://data.oaklandca.gov/dataset/Campaign-Finance-FPPC-Form-460-Schedule-C-Non-Mone/ba44-jqtm'
+
+#late contributions
+late_contributions = 'https://data.oaklandca.gov/dataset/Campaign-Finance-FPPC-Form-497-Late-Contribution-R/qact-u8hq'
+
+#summary totals (balance sheet)
+summary_totals = 'https://data.oaklandca.gov/dataset/Campaign-Finance-FPPC-Form-460-Summary-Totals/rsxe-vvuw'
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import csv
 
 scheduleA_api = 'https://data.oaklandca.gov/resource/3xq4-ermg.json'
 
@@ -8,6 +9,43 @@ response = requests.get(scheduleA_api)
 scheduleA_dict = response.json()
 
 for item in scheduleA_dict:
-    print(item['filer_id'])
+    print('filer id: ' + str(item['filer_id']))
 
 
+
+#TODO: create a function that prompts user for variable name and returns a csv file containing data
+
+
+#TODO: google how to export library data to csv file
+
+#def dataSearch():
+ #   response_input = requests.get('Enter table name: ')
+  #  api_dict = response_input.json()
+
+
+
+  #  fieldnames = ['filer_id', 'filer_naml',
+   ##     'report_num','committee_type','rpt_date',
+     #   'from_date','thru_date','rec_type',
+      #  'form_type','tran_id','entity_cd',
+       # 'tran_naml','tran_namf','tran_city',
+        #'tran_state','tran_zip4','tran_emp',
+        #'tran_occ','tran_self','tran_date',
+        #'tran_amt1','tran_amt2','intr_self',
+        #'memo_refno','tran_location','elect_date']
+ #   writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+ #   writer.writeheader()
+  #  for item in scheduleA_dict:
+  #      writer.writerow(item['filer_id'])
+   #     writer.writerow(item['filer_naml'])
+    #    writer.writerow(item['report_num'])
+      #
+
+with open('data.csv','w',newline='') as csvfile:
+    
+    datawriter = csv.writer(csvfile,delimiter=' ',quotechar='|',
+                                quoting=csv.QUOTE_MINIMAL,)
+    datawriter.writerow('FilerID')
+
+    for item in scheduleA_dict:
+        datawriter.writerow(item['filer_id'])
