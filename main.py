f23952d309d522f3963c86bb463bae355c9e9855
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2020-10-14 09:45:54 -0700

    added a function getAPI()

--- main.py
+++ main.py
@@ -5,8 +5,22 @@ import pandas as pd
 from csv import reader
 from csv import writer
 
+#function that requests API link
+def getAPI():
+    print("Hello! This program will output CSV files from Socrata,\n")
+    print("at this point in time, please only use API from the API\n")
+    print("section of data.oaklandca.gov")
+    print("               ")
+    api = input("Paste the link to the API here:  ")
 
-df = pd.read_json('https://data.oaklandca.gov/resource/3xq4-ermg.json')
-df.head(5)
+    df = pd.read_json(api)
+    df.head(5)
+    print(df)
+    print(df.describe())
 
-print(df)
+#Currently only works for itemized cash contributions.
+getAPI()
+
+
+
+
